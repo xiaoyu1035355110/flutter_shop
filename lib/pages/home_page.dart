3cc58d25de42b189cb04957830ca1d89368c9faa
ThankGod <1035355110@qq.com>
2019-04-10 18:05:35 +0800

    使用dio调用简单请求

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,12 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:dio/dio.dart';
 
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    getHttp(); //执行getHttp()
     return Scaffold(
       body: Center(
         child: Text('首页'),
       ),
     );
+  }
+
+  void getHttp() async {
+    try {
+      Response response; //声明一个服务器返回的变量为Response类型
+      response = await Dio().post('https://www.easy-mock.com/mock/5c7504f63e792633f82eb74a/thankgod/post_flutter?name=张三');
+      return print(response);
+    } catch (e) {
+      return print(e);
+    }
   }
 }
