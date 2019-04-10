0959e88385f2bb39663d1e501e4031954cd3b74e
ThankGod <1035355110@qq.com>
2019-04-10 17:32:57 +0800

    底部导航栏切换效果

--- lib/pages/index_page.dart
+++ lib/pages/index_page.dart
@@ -1,12 +1,28 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
 
+import 'home_page.dart';
+import 'category_page.dart';
+import 'cart_page.dart';
+import 'member_page.dart';
+
 class IndexPage extends StatefulWidget {
   @override
   _IndexPageState createState() => _IndexPageState();
 }
 
 class _IndexPageState extends State<IndexPage> {
+
+  int currentIndex = 0;
+  var currentPage;
+
+  //定义变量接收引入的页面
+  final List tabBodies = [
+    HomePage(),
+    CategoryPage(),
+    CartPage(),
+    MemberPage()
+  ];
 
   final List<BottomNavigationBarItem> bottomTabs = [
     BottomNavigationBarItem(
@@ -28,14 +44,32 @@ class _IndexPageState extends State<IndexPage> {
   ];
 
   @override
+  void initState() {
+    super.initState();
+    setState(() {
+      currentPage = tabBodies[currentIndex];
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('百姓生活'),
         elevation: 0.0,
       ),
-      body: Center(
-        child: Text('百姓生活+'),
+      body: currentPage,
+      backgroundColor: Color.fromRGBO(244, 245, 245, 1.0),
+      bottomNavigationBar: BottomNavigationBar( //底部导航栏
+        type: BottomNavigationBarType.fixed,
+        currentIndex: currentIndex,
+        items: bottomTabs,
+        onTap: (index) {
+          setState(() {
+           currentIndex = index; 
+           currentPage = tabBodies[currentIndex];
+          });
+        },
       ),
     );
   }
