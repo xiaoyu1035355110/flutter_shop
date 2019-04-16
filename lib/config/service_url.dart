085b003a90a5ffd5f42aab8b3cadd0f2661cb5e2
ThankGod <1035355110@qq.com>
2019-04-16 17:39:56 +0800

    添加首页热卖商品接口地址

--- lib/config/service_url.dart
+++ lib/config/service_url.dart
@@ -1,4 +1,5 @@
 const serviceUrl = 'http://v.jspang.com:8088/baixing/';//此处为异步接口配置
 const servicePath = { //设置异步请求路径
   'homePageContext': serviceUrl + 'wxmini/homePageContent', //商店首页信息
+  'homePageBelowConten': serviceUrl + 'wxmini/homePageBelowConten', //商店首页热卖商品
 };
