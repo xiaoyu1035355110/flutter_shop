91a55f2897e8f6fa962f2f3b2b44518ff27f9521
ThankGod <1035355110@qq.com>
2019-04-16 17:40:20 +0800

    添加首页热卖商品接口方法

--- lib/service/service_method.dart
+++ lib/service/service_method.dart
@@ -24,3 +24,22 @@ Future getHomePageContent() async {
     return print('Error: =========> $e');
   }
 }
+
+//商店首页热卖商品
+Future homePageBelowConten() async {
+  try {
+    print('获取首页数据.........');
+    Response response;
+    Dio dio = new Dio();
+    dio.options.contentType = ContentType.parse('application/x-www-form-urlencoded');
+    int page = 1;
+    response = await dio.post(servicePath['homePageBelowConten'], data: page);
+    if (response.statusCode == 200) {
+      return response.data;
+    } else {
+      throw Exception('后端接口异常');
+    }
+  } catch (e) {
+    return print('Error: =========> $e');
+  }
+}
