71e2cb4d113073c3985d1488f325d7797b3529c4
ThankGod <1035355110@qq.com>
2019-04-16 18:05:56 +0800

    添加通用接口并且合并获取首页与热卖商品接口

--- lib/service/service_method.dart
+++ lib/service/service_method.dart
@@ -3,18 +3,20 @@ import 'dart:async';
 import 'dart:io';
 import '../config/service_url.dart';
 
-//获取首页主体内容
-Future getHomePageContent() async {
+//获取通用请求
+Future request(url, formData) async {
   try {
-    print('获取首页数据.........');
+    print('获取数据.........');
     Response response;
     Dio dio = new Dio();
     dio.options.contentType = ContentType.parse('application/x-www-form-urlencoded');
-    var formData = {
-      'lon': '115.02932',
-      'lat': '35.76189'
-    };
-    response = await dio.post(servicePath['homePageContext'], data: formData);
+
+    if (formData == null) {
+      response = await dio.post(servicePath[url]);
+    } else {
+      response = await dio.post(servicePath[url], data: formData);
+    }
+
     if (response.statusCode == 200) {
       return response.data;
     } else {
@@ -25,21 +27,3 @@ Future getHomePageContent() async {
   }
 }
 
-//商店首页热卖商品
-Future getHomePageBelowConten() async {
-  try {
-    print('获取首页数据.........');
-    Response response;
-    Dio dio = new Dio();
-    dio.options.contentType = ContentType.parse('application/x-www-form-urlencoded');
-    int page = 1;
-    response = await dio.post(servicePath['homePageBelowConten'], data: page);
-    if (response.statusCode == 200) {
-      return response.data;
-    } else {
-      throw Exception('后端接口异常');
-    }
-  } catch (e) {
-    return print('Error: =========> $e');
-  }
-}
