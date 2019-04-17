ae1a734ce8051db5096dbcec50361d1b0e9b6d34
ThankGod <1035355110@qq.com>
2019-04-18 06:50:20 +0800

    调试商品类别接口

--- lib/pages/category_page.dart
+++ lib/pages/category_page.dart
@@ -2,13 +2,26 @@ import 'package:flutter/material.dart';
 import 'dart:convert';
 import '../service/service_method.dart';
 
-class CategoryPage extends StatelessWidget {
+class CategoryPage extends StatefulWidget {
+  @override
+  _CategoryPageState createState() => _CategoryPageState();
+}
+
+class _CategoryPageState extends State<CategoryPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Text('分类'),
+  _getCategory();
+    return  Container(
+      child: Center(
+        child: Text('分类商品'),
       ),
     );
+  }
+
+  void _getCategory(){
+    request('getCategory').then((val){
+      var data = json.decode(val.toString());
+      print(data);
+    });
   }
 }
