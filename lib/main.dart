e1e1264824cc0d54785af79f0799256853b03c86
ThankGod <1035355110@qq.com>
2019-05-07 09:33:59 +0800

    添加全局状态管理

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,20 @@
 import 'package:flutter/material.dart';
 import 'pages/index_page.dart';
+import 'package:provide/provide.dart';
+import './provide/counter.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  var counter = Counter();
+  var providers = Providers();//管理状态
+  providers
+    ..provide(Provider<Counter>.value(counter));//管理Counter泛型的状态
+  return runApp(
+    ProviderNode( //根据监听通知改变状态值
+      child: MyApp(),
+      providers: providers, //状态管理
+    )
+  );
+}
 
 class MyApp extends StatelessWidget {
   @override
