775382b26bd8c93af3cc35000f2cd1a16bd431d5
ThankGod <1035355110@qq.com>
2019-05-07 09:21:18 +0800

    搭建案例练习结构

--- lib/pages/cart_page.dart
+++ lib/pages/cart_page.dart
@@ -5,7 +5,47 @@ class CartPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Text('购物车'),
+        child: Column(
+          children: <Widget>[
+            Number(),
+            MyButton()
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class Number extends StatefulWidget {
+  @override
+  _NumberState createState() => _NumberState();
+}
+
+class _NumberState extends State<Number> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.only(top: 200),
+      child: Text(
+        '0',
+        style: Theme.of(context).textTheme.display1,
+      ),
+    );
+  }
+}
+
+class MyButton extends StatefulWidget {
+  @override
+  _MyButtonState createState() => _MyButtonState();
+}
+
+class _MyButtonState extends State<MyButton> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: RaisedButton(
+        onPressed: (){},
+        child: Text('递增'),
       ),
     );
   }
