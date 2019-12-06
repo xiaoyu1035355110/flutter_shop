171b2b44171fad28e11ac21d1d3549ab13ed6cbd
ThankGod <1035355110@qq.com>
2019-12-06 15:59:03 +0800

    自定义详情页Tabbar Widget

--- lib/provide/details_info.dart
+++ lib/provide/details_info.dart
@@ -6,6 +6,21 @@ import '../model/details.dart';
 class DetailsInfoProvide with ChangeNotifier {
   DetailsModel goodsInfo = null;
 
+  bool isLeft = true;
+  bool isRight = false;
+
+  //tabbar 切换
+  changeLeftAndRight(String changeState) {
+    if (changeState == 'left') {
+      isLeft = true;
+      isRight = false;
+    } else {
+      isLeft = false;
+      isRight = true;
+    }
+    notifyListeners();
+  }
+
   //从后台获取数据
   getGoodsInfo(String id) {
     var formData = {'goodId': id};
