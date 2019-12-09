d630097603e72fade1baf82b7e6b0d3395d745be
ThankGod <1035355110@qq.com>
2019-12-09 16:03:28 +0800

    详细页_详情和评论的切换

--- lib/pages/details_page/details_web.dart
+++ lib/pages/details_page/details_web.dart
@@ -10,10 +10,24 @@ class DetailsWeb extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var goodsInfo = Provide.value<DetailsInfoProvide>(context).goodsInfo.data.goodInfo.goodsDetail;
-    return Container(
-      child: Html(
-        data: goodsInfo
-      ),
+    return Provide<DetailsInfoProvide>(
+      builder: (context, child, val) {
+        var isLeft = Provide.value<DetailsInfoProvide>(context).isLeft;
+        if(isLeft) {
+          return Container(
+            child: Html(
+              data: goodsInfo
+            ),
+          );
+        } else {
+          return Container(
+            width: ScreenUtil().setWidth(750),
+            padding: EdgeInsets.all(10.0),
+            alignment: Alignment.center,
+            child: Text('当前暂无数据'),
+          );
+        }
+      }
     );
   }
 }
