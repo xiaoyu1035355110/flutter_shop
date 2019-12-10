167a286bbc35ef1c8c13cccd9e9bf38cdb8b2842
ThankGod <1035355110@qq.com>
2019-12-10 11:35:01 +0800

    购物车_数据模型建立和Provide修改

--- lib/provide/cart.dart
+++ lib/provide/cart.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'dart:convert';
+import '../model/cartInfo.dart';
 
 class CartProvide with ChangeNotifier {
   String cartString = "[]";
+  List<CartInfoModel> cartList = [];
 
   save(goodsId, goodsName, count, price, images) async {
     //初始化持久化配置
@@ -24,6 +26,7 @@ class CartProvide with ChangeNotifier {
       if (item['goodsId'] == goodsId) {
         //如果存在，数量进行+1操作
         tempList[iVal]['count'] = item['count'] + 1;
+        cartList[iVal].count++;
         isHave = true;
       }
 
@@ -32,26 +35,51 @@ class CartProvide with ChangeNotifier {
 
     //如果没有进行添加
     if(!isHave) {
-      tempList.add({
+      Map<String, dynamic> newGoods = {
         'goodsId': goodsId,
         'goodsName': goodsName,
         'count': count,
         'price': price,
         'iamges': images
-      });
+      };
+      tempList.add(newGoods);
+      //将商品转换为CartInfoModel类型
+      cartList.add(new CartInfoModel.fromJson(newGoods));
     }
 
     //把字符串进行encode操作
     cartString = json.encode(tempList).toString();
-    print(cartString);
+    print('字符串>>>>>>>>>>>>>>>>> ${cartString}');
+    print('model类型>>>>>>>>>>>>>>>>> ${cartList}');
     prefs.setString('cartInfo', cartString); //进行持久化
     notifyListeners();
   }
 
+  //清空购物车
   remove() async {
     SharedPreferences prefs = await SharedPreferences.getInstance();
     prefs.remove('cartInfo');
+    cartList = [];
     print('清空完成-----------------------');
+    notifyListeners();
+  }
+
+  //获取购物车列表
+  getCartInfo() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    //获得购物车商品 这时候是一个字符串
+    cartString = prefs.getString('cartInfo');
+    //把cartList进行初始化防止数据混乱
+    cartList = [];
+    //判断得到的字符串是否有值,如果不判断会报错
+    if(cartString == null) {
+      cartList = [];
+    } else {
+      List<Map> tempList = (json.encode(cartString.toString()) as List).cast();
+      tempList.forEach((item) {
+        cartList.add(new CartInfoModel.fromJson(item));
+      });
+    }
     notifyListeners();
   }
 }
