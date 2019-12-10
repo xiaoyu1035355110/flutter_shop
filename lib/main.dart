ee7c5dbe98ef0d2d92079f783e5ce6b8d4463b0f
ThankGod <1035355110@qq.com>
2019-12-10 10:41:30 +0800

    购物车添加商品

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,7 @@ import './provide/counter.dart';
 import './provide/child_category.dart';
 import './provide/category_goods_list.dart';
 import './provide/details_info.dart';
+import './provide/cart.dart';
 import 'package:fluro/fluro.dart';
 import './routers/routes.dart';
 import './routers/application.dart';
@@ -15,6 +16,7 @@ void main() {
   var providers = Providers();//管理状态
   var categoryGoodsListProvide = CategoryGoodsListProvide();
   var detailsInfoProvide = DetailsInfoProvide();
+  var cartProvide = CartProvide();
 
   //注册全局路由
   final router = Router();
@@ -25,6 +27,7 @@ void main() {
     ..provide(Provider<Counter>.value(counter))//管理Counter泛型的状态
     ..provide(Provider<CategoryGoodsListProvide>.value(categoryGoodsListProvide))//管理Counter泛型的状态
     ..provide(Provider<DetailsInfoProvide>.value(detailsInfoProvide))//管理Counter泛型的状态
+    ..provide(Provider<CartProvide>.value(cartProvide))//管理Counter泛型的状态
     ..provide(Provider<ChildCategory>.value(childCategory));//管理Counter泛型的状态
   return runApp(
     ProviderNode( //根据监听通知改变状态值
