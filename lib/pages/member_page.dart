8c62c267d09c31ed4ca9c7238f7f7e8603e681f0
ThankGod <1035355110@qq.com>
2019-12-12 11:50:01 +0800

    会员中心_首页头部布局

--- lib/pages/member_page.dart
+++ lib/pages/member_page.dart
@@ -1,23 +1,51 @@
 import 'package:flutter/material.dart';
-import 'package:provide/provide.dart';
-import '../provide/counter.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class MemberPage extends StatelessWidget {
+  const MemberPage({Key key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Container(
-          padding: EdgeInsets.only(top: 200),
-          child: Provide<Counter>( //使用Provide指定泛型利用builder输出数据
-            builder: (context, child, counter) {
-              return Text(
-                '${counter.value}',
-                style: Theme.of(context).textTheme.display1,
-              );
-            },
+      appBar: AppBar(
+        title: Text('会员中心'),
+      ),
+      body: ListView(
+        children: <Widget>[
+          _topHeader(),
+        ],
+      )
+    );
+  }
+
+  Widget _topHeader() {
+    return Container(
+      width: ScreenUtil().setWidth(750),
+      padding: EdgeInsets.all(20.0),
+      color: Colors.pinkAccent,
+      child: Column(
+        children: <Widget>[
+          Container(
+            margin: EdgeInsets.only(
+              top: 30.0
+            ),
+            child: ClipOval(
+              child: Image.network('http://blogimages.jspang.com/blogtouxiang1.jpg'),
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.only(
+              top: 10.0
+            ),
+            child: Text(
+              '技术胖',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: ScreenUtil().setSp(36)
+              )
+            ),
           )
-        ),
+        ],
       ),
     );
   }
